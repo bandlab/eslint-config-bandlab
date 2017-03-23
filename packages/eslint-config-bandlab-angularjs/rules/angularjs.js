1fe6f117d4fd0aebf810d3f8079c30ff29034291
Laurent Le Graverend <laurent@legraverend.fr>
2017-03-23 17:31:09 +0800

    Update to eslint-plugin v2 + add rules

--- packages/eslint-config-bandlab-angularjs/rules/angularjs.js
+++ packages/eslint-config-bandlab-angularjs/rules/angularjs.js
@@ -3,46 +3,64 @@ module.exports = {
     'angular'
   ],
   rules: {
-    'angular/angularelement': 2,
+    // Possible Errors
+    'angular/module-getter': 2,
+    'angular/module-setter': 2,
+    'angular/no-private-call': 0,
+
+    // Best Practises
+    'angular/component-limit': [0, 1],
     'angular/controller-as': 0,
     'angular/controller-as-route': 0,
     'angular/controller-as-vm': [2, 'vm'],
-    'angular/controller-name': [2, '/[A-Z].*Ctrl$/'],
     'angular/deferred': 0,
-    'angular/definedundefined': 2,
-    'angular/di': [2, 'function'],
-    'angular/di-order': [2, true, 'case_insensitive'],
     'angular/di-unused': 2,
-    'angular/directive-name': 0,
     'angular/directive-restrict': [0, { 'restrict': 'AE', 'explicit': 'never' }],
-    'angular/component-limit': [0, 1],
-    'angular/document-service': 0,
     'angular/empty-controller': 0,
+    'angular/no-controller': 0,
+    'angular/no-inline-template': [0, { 'allowSimple': true }],
+    'angular/no-run-logic': [0, { 'allowParams': true }],
+    'angular/no-services': [2, ['$resource']],
+    'angular/on-watch': 0,
+
+    // Deprecated Features
+    'angular/no-cookiestore': 2,
+    'angular/no-directive-replace': 0,
+    'angular/no-http-callback': 2,
+
+    // Naming
+    'angular/component-name': 0,
+    'angular/constant-name': 0,
+    'angular/controller-name': [2, '/[A-Z].*Ctrl$/'],
+    'angular/directive-name': 0,
+    'angular/factory-name': 0,
     'angular/file-name': 0,
     'angular/filter-name': 0,
-    'angular/foreach': 0,
+    'angular/module-name': 0,
+    'angular/provider-name': 0,
+    'angular/value-name': 0,
+
+    // Conventions
+    'angular/di': [2, 'function'],
+    'angular/di-order': [2, true, 'case_insensitive'],
+    'angular/dumb-inject ': 2,
     'angular/function-type': 0,
+    'angular/no-service-method': 2,
+    'angular/module-dependency-order': [0, { 'grouped': true, 'prefix': null }],
+    'angular/one-dependency-per-line': 0,
+    'angular/rest-service': 0,
+    'angular/watchers-execution': [0, '$digest'],
+
+    // Angular Wrappers
+    'angular/angularelement': 2,
+    'angular/definedundefined': 2,
+    'angular/document-service': 0,
+    'angular/foreach': 0,
     'angular/interval-service': 0,
     'angular/json-functions': 0,
     'angular/log': 0,
-    'angular/module-dependency-order': [0, { 'grouped': true, 'prefix': null }],
-    'angular/module-getter': 2,
-    'angular/module-name': 0,
-    'angular/module-setter': 2,
     'angular/no-angular-mock': 0,
-    'angular/no-controller': 0,
-    'angular/no-cookiestore': 2,
-    'angular/no-http-callback': 2,
     'angular/no-jquery-angularelement': 2,
-    'angular/no-inline-template': [0, { 'allowSimple': true }],
-    'angular/no-private-call': 0,
-    'angular/no-run-logic': [0, { 'allowParams': true }],
-    'angular/no-service-method': 2,
-    'angular/no-services': [2, ['$resource']],
-    'angular/on-watch': 0,
-    'angular/one-dependency-per-line': 0,
-    'angular/rest-service': 0,
-    'angular/service-name': 2,
     'angular/timeout-service': 0,
     'angular/typecheck-array': 2,
     'angular/typecheck-date': 2,
@@ -50,7 +68,9 @@ module.exports = {
     'angular/typecheck-number': 2,
     'angular/typecheck-object': 2,
     'angular/typecheck-string': 2,
-    'angular/watchers-execution': [0, '$digest'],
-    'angular/window-service': 0
+    'angular/window-service': 0,
+
+    // Misspelling
+    'angular/on-destroy': 2,
   }
 };
