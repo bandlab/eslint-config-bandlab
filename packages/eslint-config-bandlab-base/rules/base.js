632ea52bf8c84dc3250dddc2a64d14eb905f7668
Gilles Piou <gilles.piou@gmail.com>
2018-03-07 18:27:34 +0800

    Enforce no-else-return rule (#16)

--- packages/eslint-config-bandlab-base/rules/base.js
+++ packages/eslint-config-bandlab-base/rules/base.js
@@ -29,6 +29,7 @@ module.exports = {
     'no-caller': 2,
     'no-catch-shadow': 2,
     'no-console': 0,
+    'no-else-return': 2,
     'no-eval': 2,
     'no-extend-native': 2,
     'no-extra-bind': 1,
